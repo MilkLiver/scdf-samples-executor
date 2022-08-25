97373cd2bfa67a30d796bc0721c966a40189084d
neil.yu <neil.yu@dynasafe.com.tw>
2022-08-25 10:29:39 +0800

    change send result from use api to use kafka

--- src/main/java/com/milkliver/samples/scdfexecutor01/task/MainTask.java
+++ src/main/java/com/milkliver/samples/scdfexecutor01/task/MainTask.java
@@ -15,6 +15,8 @@ import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.milkliver.samples.scdfexecutor01.kafka.MessageProducer;
 import com.milkliver.samples.scdfexecutor01.utils.SendRequest;
 
 @Component
@@ -53,6 +55,9 @@ public class MainTask {
 	@Value("${spring.cloud.task.executionid:#{null}}")
 	String taskid;
 
+	@Autowired
+	MessageProducer messageProducer;
+
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
@@ -65,7 +70,6 @@ public class MainTask {
 			}
 
 			try {
-
 				log.info("CommandBase64: " + systemCommandBase64);
 				String systemCommand = new String(decoder.decode(systemCommandBase64));
 				log.info("Command: " + systemCommand);
@@ -89,44 +93,49 @@ public class MainTask {
 
 				log.info("=========================end=========================");
 
-				Map<String, Object> jsonMap = new HashMap<String, Object>();
-
-				jsonMap.put("message", taskid);
+				Map<String, Object> taskExecuteResMsgMap = new HashMap<String, Object>();
+				Map<String, Object> batchInfoMap = new HashMap<String, Object>();
+				boolean taskExecuteStatus = false;
 
 				if (process.waitFor() != 0) {
 					log.info("task is failed");
 				} else {
-					Map<String, Object> sendSuccessMsgRes = new HashMap<String, Object>();
-					if (scdfServerRuntaskApiRequestEnableHttps) {
-						try {
-							sendSuccessMsgRes = sendRequest.https(scdfServerRuntaskApiRequestUrl,
-									scdfServerRuntaskApiRequestHostname, scdfServerRuntaskApiRequestMethod,
-									scdfServerRuntaskApiRequestConnectTimeOut, scdfServerRuntaskApiRequestReadTimeOut,
-									jsonMap);
-						} catch (Exception e) {
-							log.error(e.getMessage());
-							for (StackTraceElement elem : e.getStackTrace()) {
-								log.error(elem.toString());
-							}
-						}
-
-					} else {
-						try {
-							sendSuccessMsgRes = sendRequest.http(scdfServerRuntaskApiRequestUrl,
-									scdfServerRuntaskApiRequestHostname, scdfServerRuntaskApiRequestMethod,
-									scdfServerRuntaskApiRequestConnectTimeOut, scdfServerRuntaskApiRequestReadTimeOut,
-									jsonMap);
-						} catch (Exception e) {
-							log.error(e.getMessage());
-							for (StackTraceElement elem : e.getStackTrace()) {
-								log.error(elem.toString());
-							}
-						}
-
-					}
-
-					log.info("response content: " + String.valueOf(sendSuccessMsgRes.get("responseContent")));
+					taskExecuteStatus = true;
+//					sendResultApi();
 					log.info("task is success");
+				}
+
+				// create json string
+				String execCmdResLogsBase64 = new String(encoder.encode(execCmdRes.toString().getBytes()));
+
+				taskExecuteResMsgMap.put("status", taskExecuteStatus);
+
+				batchInfoMap.put("logs", execCmdResLogsBase64);
+
+				batchInfoMap.put("command", systemCommandBase64);
+				taskExecuteResMsgMap.put("batch", batchInfoMap);
+
+				if (taskid != null) {
+					taskExecuteResMsgMap.put("taskid", taskExecuteStatus);
+					log.info("taskid: " + String.valueOf(taskid) + " is running ...");
+				} else {
+					taskExecuteResMsgMap.put("taskid", "-1");
+					log.info("taskid: null is running ...");
+				}
+				ObjectMapper taskExecuteResMsgJson = new ObjectMapper();
+				StringBuilder taskExecuteResMsgJsonStrSb = new StringBuilder();
+
+				taskExecuteResMsgJsonStrSb.append(taskExecuteResMsgJson.writeValueAsString(taskExecuteResMsgMap));
+
+				// send json to kafka
+				log.info("send to kafka json: " + taskExecuteResMsgJsonStrSb.toString());
+				try {
+					messageProducer.send(taskExecuteResMsgJsonStrSb.toString());
+				} catch (Exception e) {
+					log.error(e.getMessage());
+					for (StackTraceElement elem : e.getStackTrace()) {
+						log.error(elem.toString());
+					}
 				}
 
 				process.destroy();
@@ -138,6 +147,7 @@ public class MainTask {
 				}
 			}
 			log.info("CommandLineRunner finish");
+
 			if (taskid != null) {
 				log.info("taskid: " + String.valueOf(taskid) + " is finished ...");
 			} else {
@@ -145,6 +155,39 @@ public class MainTask {
 			}
 			log.info("SCDF executor CommandLineRunner finish");
 		};
+	}
+
+	public void sendResultApi() {
+
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		jsonMap.put("message", taskid);
+
+		Map<String, Object> sendSuccessMsgRes = new HashMap<String, Object>();
+		if (scdfServerRuntaskApiRequestEnableHttps) {
+			try {
+				sendSuccessMsgRes = sendRequest.https(scdfServerRuntaskApiRequestUrl,
+						scdfServerRuntaskApiRequestHostname, scdfServerRuntaskApiRequestMethod,
+						scdfServerRuntaskApiRequestConnectTimeOut, scdfServerRuntaskApiRequestReadTimeOut, jsonMap);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				for (StackTraceElement elem : e.getStackTrace()) {
+					log.error(elem.toString());
+				}
+			}
+
+		} else {
+			try {
+				sendSuccessMsgRes = sendRequest.http(scdfServerRuntaskApiRequestUrl,
+						scdfServerRuntaskApiRequestHostname, scdfServerRuntaskApiRequestMethod,
+						scdfServerRuntaskApiRequestConnectTimeOut, scdfServerRuntaskApiRequestReadTimeOut, jsonMap);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				for (StackTraceElement elem : e.getStackTrace()) {
+					log.error(elem.toString());
+				}
+			}
+		}
+		log.info("response content: " + String.valueOf(sendSuccessMsgRes.get("responseContent")));
 	}
 
 }
